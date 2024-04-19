6fdcf2fa920f9635fc0a1dd47c06ff622a73132b
ZaibLComrade <kmjahanzaib@proton.me>
2024-04-19 19:21:51 +0600

    solved max number occurance

--- Toph/easy/owllen.cpp
+++ Toph/easy/owllen.cpp
@@ -1,8 +1,38 @@
 // https://toph.co/p/icpc-is-coming-in-hot
 
 #include <iostream>
+#define MAXINP 6
+#define BASE 10
 using namespace std;
 
+int countMaxOccurance(char num[]) {
+	int maxNum;
+	int maxOccurance = 0;
+	int occurance[BASE];
+	
+	// Initializing all indexes representing occurance
+	for(int i = 0; i < BASE; i++) occurance[i] = 0;
+	
+	// Counting occurances
+	int i = 0;
+	while(num[i] != '\0') {
+		int idx = num[i++] - '0';
+		occurance[idx]++;
+		if(maxOccurance < occurance[idx]) {
+			maxOccurance = occurance[idx]; 
+			maxNum = idx;
+		}
+	}
+	
+	return maxNum;
+}
+
 int main() {
+	char num[MAXINP];
+	cin >> num;
+	
+	int maxNum = countMaxOccurance(num);
+	cout << maxNum << endl;
+	
 	return 0;
 }
