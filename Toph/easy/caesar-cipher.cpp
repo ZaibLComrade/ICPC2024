2870e6a1afd45ce9cc1b7fab9cbe8558944ccdad
ZaibLComrade <kmjahanzaib@proton.me>
2024-04-20 21:26:18 +0600

    solved caesar cipher

--- Toph/easy/caesar-cipher.cpp
+++ Toph/easy/caesar-cipher.cpp
@@ -1,26 +1,48 @@
 // https://toph.co/p/caesar-cipher
 
 #include <iostream>
-#define MAXLEN 128
+#define MAXBUFFER 128
+#define MAXLEN MAXBUFFER
 #define ALPHALIMIT 26
 using namespace std;
+enum Alpha {
+	Capital,
+	Small,
+	NonAlpha
+};
+
+Alpha getAlphaType(char c) {
+	if(c >= 'A' && c <= 'Z') return Capital;
+	else if(c >= 'a' && c <= 'z') return Small;
+	return NonAlpha;
+}
 
 int main() {
-	char plainText[MAXLEN];
+	char plainText[MAXBUFFER];
 	char cipherText[MAXLEN];
 	int shift = 3;
-	cin >> plainText;
 	
-	for(int i = 0; plainText[i] != '\0'; i++) {
-		char shiftedChar = plainText[i] + shift;
-		/* cout << shiftedChar << endl; */
-		shiftedChar %= ALPHALIMIT;
-		cout << shiftedChar << endl;
-		cipherText[i] = shiftedChar;
+	cin.getline(plainText, MAXBUFFER);
+	
+	for(int i = 0, j = 0; plainText[i] != '\0'; i++) {
+		char c = plainText[i];
+		Alpha charType = getAlphaType(c);
+		int shiftedChar = c;
+		if(charType != NonAlpha) {
+			cout << "unshifted " << shiftedChar;
+			if(charType == Capital) shiftedChar = ((c - 'A' + shift) % ALPHALIMIT) + 'A';
+			else shiftedChar = ((c - 'a' + shift) % ALPHALIMIT) + 'a';
+			cout << " shifted " << shiftedChar << endl;
+			cipherText[j] = static_cast<char>(shiftedChar);
+		} else {
+			cipherText[j] = c;
+		}
+		++j;
+		cipherText[j] = '\0';
 	}
 	
 	for(int i = 0; cipherText[i] != '\0'; i++) {
-		cout << cipherText;
+		cout << cipherText << endl;
 	}
 	
 	return 0;
