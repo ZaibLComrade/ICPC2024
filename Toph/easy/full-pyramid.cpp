61a3ca9a4ce721819aa5296c119b0214ec4d4910
ZaibLComrade <kmjahanzaib@proton.me>
2024-04-22 18:08:56 +0600

    solved atcoder add and mult

---
+++ Toph/easy/full-pyramid.cpp
@@ -0,0 +1,16 @@
+// https://toph.co/p/full-pyramid
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	int len = 2 * n - 1;
+	for(int i = 0; i < n; i++) {
+		if(i )
+	}
+	
+	return 0;
+}
